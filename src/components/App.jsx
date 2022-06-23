e4061802f8b1b3bfeb0725f9f89b5a1a4ef348c7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-23 23:56:00 +0300

    import + render Footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from 'react';
+import Footer from './Footer';
 import './App.scss';
 
 function App() {
   const [quote, setQuote] = useState();
   useEffect(() => {
-    fetch('https://api.adviceslip.com/advice').then(response => response.json()).then(data => setQuote(data.slip.advice))
-  }, [])
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((data) => setQuote(data.slip.advice));
+  }, []);
 
   return (
-    <div className='attribution'>
-      Challenge by{' '}
-      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
-        Frontend Mentor
-      </a>
-      . Coded by <a href='https://github.com/pronoia9'>Jay Ansin</a>.
-    </div>
+    <>
+      <Footer />
+    </>
   );
 }
 
