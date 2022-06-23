594fb48834157afed35ab2e2d9dbfe38cadfea2a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-24 00:31:15 +0300

    use quote state as quote

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({ icon, quote, buttonClick }) {
   return (
     <main className='card'>
       <h1 className='card__number'>Advice #117</h1>
-      <p className='card__quote'>&#8220It is easy to sit up and take notice, what's difficult is getting up and taking action.&#8221</p>
+      <p className='card__quote'>{quote}</p>
       <div className='card__dice' onClick={buttonClick}>
         <img src={icon.default} alt='dice' />
       </div>
