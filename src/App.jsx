74c1268a5d9072f758d9dcf54aef9dcb6d0b713d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-23 23:37:40 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div class='attribution'>
+      Challenge by{' '}
+      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
+        Frontend Mentor
+      </a>
+      . Coded by <a href='https://github.com/pronoia9'>Jay Ansin</a>.
+    </div>
+  );
+}
+
+export default App;
