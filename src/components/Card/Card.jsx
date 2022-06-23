6e0652204f9064d14d97b4c1d4be7dfba0039806
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-24 00:29:45 +0300

    use prop icon as img src

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,12 @@
 import './Card.scss';
 
-export default function Card({ image, quote, buttonClick }) {
+export default function Card({ icon, quote, buttonClick }) {
   return (
     <main className='card'>
       <h1 className='card__number'>Advice #117</h1>
       <p className='card__quote'>&#8220It is easy to sit up and take notice, what's difficult is getting up and taking action.&#8221</p>
       <div className='card__dice'>
-        <img src='../../images/icon-dice.svg' alt='dice' />
+        <img src={icon.default} alt='dice' />
       </div>
     </main>
   );
