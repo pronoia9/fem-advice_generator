6d5b54fd642c2696b319c336be4f9cbbc2c4926a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-24 00:30:35 +0300

    add onClick = buttonClick

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({ icon, quote, buttonClick }) {
     <main className='card'>
       <h1 className='card__number'>Advice #117</h1>
       <p className='card__quote'>&#8220It is easy to sit up and take notice, what's difficult is getting up and taking action.&#8221</p>
-      <div className='card__dice'>
+      <div className='card__dice' onClick={buttonClick}>
         <img src={icon.default} alt='dice' />
       </div>
     </main>
