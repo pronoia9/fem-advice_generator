dcc2b628b1074d3fe37808778b3160edbe6b64b5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-24 00:04:36 +0300

    add initial html

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import './Card.scss';
+
+export default function Card({ quote, buttonClick }) {
+  return (
+    <main className='card'>
+      <h1 className='card__number'>Advice #117</h1>
+      <p className='card__quote'>&#8220It is easy to sit up and take notice, what's difficult is getting up and taking action.&#8221</p>
+      <div className='card__dice'>
+        <img src='../../images/icon-dice.svg' alt='dice' />
+      </div>
+    </main>
+  );
+}
