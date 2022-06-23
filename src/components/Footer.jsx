f72e76bed773bec7bbb2b779dfc4d8eed495acc1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-23 23:55:33 +0300

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className='attribution'>
+      Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>Frontend Mentor</a>. Coded by <a href='https://github.com/pronoia9'>Jay Ansin</a>.
+    </div>
+  );
+}
