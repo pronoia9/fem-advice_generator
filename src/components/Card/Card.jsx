9577140eadaddc7b535b03c277dd140713fb4fcb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-24 19:54:44 +0300

    rename classes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,9 +3,9 @@ import './Card.scss';
 export default function Card({ icon, card, buttonClick }) {
   return (
     <main className='card'>
-      <h1 className='card__number'>Advice #{card && card.id ? card.id : ''}</h1>
-      <p className='card__quote'>{card && card.advice ? card.advice : ''}</p>
-      <div className='card__dice' onClick={buttonClick}>
+      <h1 className='card_id'>Advice #{card && card.id ? card.id : ''}</h1>
+      <p className='card_advice'>{card && card.advice ? card.advice : ''}</p>
+      <div className='card_button' onClick={buttonClick}>
         <img src={icon.default} alt='dice' />
       </div>
     </main>
