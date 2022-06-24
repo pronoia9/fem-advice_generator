4e2c1d9c18ccf476e7e0c9577d3ec147f1d695a2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-24 19:36:24 +0300

    change text to card.id and card.quote

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,10 @@
 import './Card.scss';
 
-export default function Card({ icon, quote, buttonClick }) {
+export default function Card({ icon, card, buttonClick }) {
   return (
     <main className='card'>
-      <h1 className='card__number'>Advice #117</h1>
-      <p className='card__quote'>{quote}</p>
+      <h1 className='card__number'>Advice #{card && card.id ? card.id : ''}</h1>
+      <p className='card__quote'>{card && card.advice ? card.advice : ''}</p>
       <div className='card__dice' onClick={buttonClick}>
         <img src={icon.default} alt='dice' />
       </div>
