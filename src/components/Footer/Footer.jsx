df5b57246a849d077a0f6798065d795947b7dd2e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-24 20:06:26 +0300

    add data prop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import './Footer.scss';
 
-export default function Footer() {
+export default function Footer({ data }) {
   return (
     <footer className='attribution'>
-      Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>Frontend Mentor</a>. Coded by <a href='https://github.com/pronoia9'>Jay Ansin</a>.
+      Challenge by <a href={data.challengeURL} target='_blank' rel='noreferrer'>Frontend Mentor</a>. Coded by <a href={data.githubURL}>{data.name}</a>.
     </footer>
   );
 }
