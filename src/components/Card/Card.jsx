56835e46a76b060c806f6b00af79939e5515e4fa
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-25 14:21:51 -0500

    add height + width for icon

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({ icon, card, buttonClick }) {
       <h1 className='card_id'>Advice #{card && card.id ? card.id : ''}</h1>
       <p className='card_advice'>{card && card.advice ? card.advice : ''}</p>
       <div className='card_button' onClick={buttonClick}>
-        <img src={icon.default} alt='dice' />
+        <img src={icon.default} alt='dice' width='24px' height='24px' />
       </div>
     </main>
   );
