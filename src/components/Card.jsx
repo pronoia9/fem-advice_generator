d6b82e19155d3282a13725e4b1afca65f24b6376
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 18:27:21 +0300

    add inital card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,31 @@ import styled from 'styled-components';
 import { Text, Button } from '.';
 
 const Card = ({ advice, fetchAdvice }) => {
+  console.log(advice);
+
   return (
-    <div>
-      <Text />
-      <Button />
-    </div>
+    <Container>
+      <Text advice={advice} />
+      <Button fetchAdvice={fetchAdvice} />
+    </Container>
   );
 };
 
 export default Card;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #323a49;
+  position: absolute;
+  width: clamp(295px, 90%, 34rem);
+  margin-top: 7.5rem;
+  border-radius: 0.5rem;
+
+  @media (min-width: 800px) {
+    margin-top: 0;
+    margin-bottom: 2rem;
+    width: 34rem;
+  }
+`;
