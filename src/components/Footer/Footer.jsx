902b89455a8de4f1a4dfe9ecf212705b13a95b30
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 16:35:36 +0300

    vite init + cleanup of default data/files

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.scss';
-
-export default function Footer({ data }) {
-  return (
-    <footer className='attribution'>
-      Challenge by <a href={data.challengeURL} target='_blank' rel='noreferrer'>Frontend Mentor</a>. Coded by <a href={data.githubURL}>{data.name}</a>.
-    </footer>
-  );
-}
