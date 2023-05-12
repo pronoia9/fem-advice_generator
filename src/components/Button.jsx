6b42fa511e74427f27e7b2a6557078aa96fa5b46
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 18:27:26 +0300

    add initial button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,35 @@
 import styled from 'styled-components';
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ fetchAdvice }) => {
+  return (
+    <Container onClick={fetchAdvice}>
+      <img src='/icon-dice.svg' />
+    </Container>
+  );
 };
 
 export default Button;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #52ffa8;
+  height: 4rem;
+  width: 4rem;
+  border-radius: 50%;
+  position: relative;
+  bottom: -2rem;
+
+  img {
+    height: 1.5rem;
+  }
+
+  &:hover {
+    box-shadow: 0rem 0rem 1rem #52ffa8;
+    cursor: pointer;
+  }
+
+  @media (min-width: 800px) {
+  }
+`;
