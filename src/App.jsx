8080728a9e2d7f587da2164ead90da765e6483c8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 17:46:47 +0300

    fetch advice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import { Card } from './components';
 
 const App = () => {
-  return <Card />;
+  const [advice, setAdvice] = useState(null);
+
+  const fetchAdvice = async () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const data = response.data.slip;
+        advice?.id !== data.id ? setAdvice(data) : fetchAdvice();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return <Card advice={advice} setAdvice={setAdvice} />;
 };
 
 export default App;
